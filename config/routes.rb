5067c6dbe2f861a55debfdefae5bf874060f7551
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-06 16:51:31 -0600

    Add get statement to welcome index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get '/', to:'welcome#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/:id', to: 'shelters#show'
 end
