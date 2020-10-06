2d944772ade302c409173927e8683a5e0730d561
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-06 15:25:24 -0600

    Update shelters controllers and shelter model for finding shelter by id

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -2,4 +2,8 @@ class SheltersController < ApplicationController
   def index
     @shelters = ['Shelter 1', 'Shelter 2', 'Shelter 3']
   end
+
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
 end
