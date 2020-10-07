f21d2635e714aa0824cb71378c2fa21102f8c43c
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-06 20:30:53 -0600

    Add create method for creating new shelter and redirect to shelters index

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -8,5 +8,18 @@ class SheltersController < ApplicationController
   end
 
   def new
-  end 
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+
+    shelter.save
+    redirect_to '/shelters'
+  end
 end
