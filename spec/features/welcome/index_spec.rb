a8861ce74f3ce09b9ae1d4096e075d9142196a5d
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-07 18:51:38 -0600

    Create test for welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,1 +1,13 @@
 require 'rails_helper'
+describe 'As a user' do
+  describe ' when I visit the root page' do
+    it 'the user sees the landing page with links to shelters' do
+
+      visit home
+
+      save_and_open_page
+      expect(page).to have_content("Adopt, Don't Shop!")
+      expect(page).to have_link("Shelters")
+    end
+  end
+end
