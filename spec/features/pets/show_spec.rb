fe944bf2fdd08b473e4016541126912467acef9f
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-11 12:57:48 -0600

    Add spec for deleting a pet

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -35,3 +35,36 @@ describe 'Pet Show' do
     end
   end
 end
+
+describe 'As a visitor' do
+  describe 'when I visit /pets/:id' do
+    it 'has a link to delete shelter and the shelter is deleted when I click' do
+    shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                               address: '1234 Happy Lane',
+                               city: 'Hopscotch Town',
+                               state: 'Colorado',
+                               zip: 12345)
+   pet_1 = Pet.create(image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                      name: 'Skye',
+                      approx_age: 3,
+                      sex: 'Female',
+                      description: 'Shy and loveable!',
+                      adoptable: true,
+                      shelter_id: shelter_1.id)
+   pet_2 = Pet.create(image: 'https://i.ibb.co/jJK9jWN/pet-2.jpg',
+                      name: 'Gigi',
+                      approx_age: 4,
+                      sex: 'Female',
+                      description: 'Talkative and playful!',
+                      adoptable: true,
+                      shelter_id: shelter_1.id)
+
+    visit "/pets/#{pet_1.id}"
+    expect(page).to have_link("Delete Pet")
+    click_on("Delete Shelter")
+    expect(current_path).to eq("/pets")
+    expect(page).to have_no_content("#{pet_1.name}")
+    expect(page).to have_content("#{pet_2.name}")
+    end
+  end
+end
