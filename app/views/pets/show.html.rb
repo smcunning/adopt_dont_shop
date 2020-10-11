a0649f0ec0fbfb53df1332e8e7a1e89dc57a1325
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-10 18:51:10 -0600

    Add view for pet show

---
+++ app/views/pets/show.html.rb
@@ -0,0 +1,6 @@
+<h3>Meet<%= pet.name %>!</h3>
+<%= image_tag(pet.image) %>
+<p>Description <%= pet.description %></p>
+<p>Approximate Age: <%= pet.approx_age %></p>
+<p>Sex: <%= pet.sex %></p>
+<p>Adoptable: <%= pet.adoptable%></p>
