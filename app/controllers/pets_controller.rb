9ecc20c426b787805fcea966d07bc0e0645ac9f3
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-11 12:08:30 -0600

    Create action for update

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -18,6 +18,24 @@ class PetsController < ApplicationController
     redirect_to "/shelters/#{shelter.id}/pets"
   end
 
+  def edit
+    @pet = Pet.find(params[:id])
+  end
+
+  def update
+    pet = Pet.find(params[:id])
+    pet.update({
+        image: params[:image],
+        name: params[:name],
+        description: params[:description],
+        approx_age: params[:approx_age],
+        sex: params[:sex],
+        adoptable: true
+      })
+    pet.save
+    redirect_to "/pets/#{pet.id}"
+  end
+
   def pet_params
     params.permit(:image, :name, :description, :approx_age, :sex)
   end
