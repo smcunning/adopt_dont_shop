d5da499f21deab480218019a0f9bcebff425347f
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-16 18:07:52 -0600

    Add spec for best/worst user review section

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -68,3 +68,48 @@ describe "As a visitor" do
     end
   end
 end
+
+describe 'Within the highlighted reviews on user show page'do
+  it 'has a the best rating and worst rating this user has written' do
+    shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                               address: '1234 Happy Lane',
+                               city: 'Hopscotch Town',
+                               state: 'Colorado',
+                               zip: 12345)
+    user_1 = User.create!(name: "Betty",
+                        address: "123 Main st",
+                        city: "Denver",
+                        state: "CO",
+                        zip: 80111)
+    review_1 = Review.create!(title: 'Best shelter ever.',
+                              rating: 5,
+                              content: 'My new pet is the best!',
+                              image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                              shelter_id: "#{shelter_1.id}",
+                              user_id: "#{user_1.id}")
+    review_2 = Review.create!(title: 'Great place!',
+                              rating: 4,
+                              content: 'I love Mr. Mittens!',
+                              shelter_id: "#{shelter_1.id}",
+                              user_id: "#{user_1.id}")
+    review_3 = Review.create!(title: 'Will not be returning',
+                              rating: 1,
+                              content: 'No one was working',
+                              shelter_id: "#{shelter_1.id}",
+                              user_id: "#{user_1.id}")
+
+    visit "/users/#{user_1.id}"
+
+    within ".best" do
+      expect(page).to have_content(review_1.title)
+      expect(page).to have_content(review_1.rating)
+      expect(page).to have_content(review_1.content)
+    end
+
+    within ".worst" do
+      expect(page).to have_content(review_2.title)
+      expect(page).to have_content(review_2.rating)
+      expect(page).to have_content(review_2.content)
+    end
+  end
+end
