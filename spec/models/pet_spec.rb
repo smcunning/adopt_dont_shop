c83d2606c88c621a195de4a73577463a90f3c568
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-17 10:52:16 -0600

    Add pet application relationship testing

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -4,7 +4,9 @@ describe Pet, type: :model do
   describe "validations" do
     it {should validate_presence_of :name}
   end
-  describe "realtionships" do
+  describe "relationships" do
     it {should belong_to :shelter}
+    it {should have_many :pet_applications}
+    it {should have_many(:applications).through(:pet_applications)}
   end
 end
