47a5aba010df27a79a5927420dbc0790aecd0758
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-16 21:14:30 -0600

    Add model testing for best/worst review methods

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  describe "relationships" do
+    it { should have_many :reviews}
+  end
 
-end 
+  describe "instance methods" do
+     it "best_review" do
+       shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                                  address: '1234 Happy Lane',
+                                  city: 'Hopscotch Town',
+                                  state: 'Colorado',
+                                  zip: 12345)
+       user_1 = User.create!(name: "Betty",
+                           address: "123 Main st",
+                           city: "Denver",
+                           state: "CO",
+                           zip: 80111)
+       review_1 = Review.create!(title: 'Best shelter ever.',
+                                 rating: 5,
+                                 content: 'My new pet is the best!',
+                                 image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+       review_2 = Review.create!(title: 'Great place!',
+                                 rating: 4,
+                                 content: 'I love Mr. Mittens!',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+       review_3 = Review.create!(title: 'Will not be returning',
+                                 rating: 1,
+                                 content: 'No one was working',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+
+       best = {
+         title: review_1.title,
+         rating: review_1.rating,
+         content: review_1.content,
+         image: review_1.image
+       }
+
+
+       expect(user_1.best_review).to eq(best)
+     end
+
+     it "worst_review" do
+       shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                                  address: '1234 Happy Lane',
+                                  city: 'Hopscotch Town',
+                                  state: 'Colorado',
+                                  zip: 12345)
+       user_1 = User.create!(name: "Betty",
+                           address: "123 Main st",
+                           city: "Denver",
+                           state: "CO",
+                           zip: 80111)
+       review_1 = Review.create!(title: 'Best shelter ever.',
+                                 rating: 5,
+                                 content: 'My new pet is the best!',
+                                 image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+       review_2 = Review.create!(title: 'Great place!',
+                                 rating: 4,
+                                 content: 'I love Mr. Mittens!',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+       review_3 = Review.create!(title: 'Will not be returning',
+                                 rating: 1,
+                                 content: 'No one was working',
+                                 shelter_id: "#{shelter_1.id}",
+                                 user_id: "#{user_1.id}")
+
+       worst = {
+         title: review_3.title,
+         rating: review_3.rating,
+         content: review_3.content,
+         image: review_3.image
+       }
+
+
+       expect(user_1.worst_review).to eq(worst)
+     end
+  end
+end
