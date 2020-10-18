026025c91ba569775a49d0fd93b79d58ba71feec
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-18 15:06:52 -0600

    Adds logic to determine if deny approve buttons should show

--- spec/features/admin_applications/show_spec.rb
+++ spec/features/admin_applications/show_spec.rb
@@ -101,5 +101,40 @@ describe "As a visitor" do
         expect(page).to have_button("Deny")
       end
     end
+
+    it "Has a button to deny application" do
+      shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                                 address: '1234 Happy Lane',
+                                 city: 'Hopscotch Town',
+                                 state: 'Colorado',
+                                 zip: 12345)
+      user = User.create!(name: "Betty",
+                          address: "123 Main st",
+                          city: "Denver",
+                          state: "CO",
+                          zip: 80111)
+      pet_1 = Pet.create(image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                         name: 'Skye',
+                         approx_age: 3,
+                         sex: 'Female',
+                         shelter_id: shelter_1.id)
+      application = Application.create!(description: "I will take great care of Skye",
+                                        status: "In Progress",
+                                        user_id: user.id)
+      pet_application = PetApplication.create!(pet_id: pet_1.id,
+                                               user_id: user.id,
+                                               application_id: application.id)
+
+      visit "/admin/applications/#{application.id}"
+
+      within "#pet-#{pet_1.id}" do
+        click_button("Deny")
+        expect(page).to_not have_button("Deny")
+      end
+
+      within ".app-info" do
+        expect(page).to have_content("Denied")
+      end
+    end
   end
 end
