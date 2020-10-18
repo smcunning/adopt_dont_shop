b589a06470e62e986c3c02b61e67ae90635fe5bf
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-18 08:52:22 -0600

    Adds pet_count, average_rating, and application_count methods to shelter model.

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -2,4 +2,18 @@ class Shelter < ApplicationRecord
   has_many :pets, dependent: :destroy
   has_many :reviews, dependent: :destroy
   validates_presence_of :name, :address, :city, :state, :zip
+
+  def pets_count
+    self.pets.count
+  end
+
+  def average_rating
+    (self.reviews.sum {|review| review.rating}).to_f / self.reviews.count
+  end
+
+  def application_count
+    self.pets.sum do |pet|
+      pet.pet_applications.count
+    end
+  end
 end
