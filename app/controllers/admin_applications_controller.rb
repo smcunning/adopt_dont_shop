b186891e908fcc185ae03d53e43e241b26c158de
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-18 14:57:07 -0600

    Adds denied option for pet application

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -6,8 +6,13 @@ class AdminApplicationsController < ApplicationController
 
   def update
     application = Application.find(params[:id])
-    application[:status] = "Approved"
-    application.save
+    if params[:status] == "Approved"
+      application[:status] = "Approved"
+      application.save
+    elsif params[:status] == "Denied"
+      application[:status] = "Denied"
+      application.save
+    end
     redirect_to "/admin/applications/#{application.id}"
   end
 end
