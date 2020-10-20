8dd15f5da2fd78d1ba12f066a8df3e89ae40e7f9
Shaun James <ShaunDaneJames@Gmail.com>
2020-10-20 16:18:20 -0600

    Adds model test for pets to know if they have pending apps

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -9,4 +9,56 @@ describe Pet, type: :model do
     it {should have_many :pet_applications}
     it {should have_many(:applications).through(:pet_applications)}
   end
+
+  it "knows if apps are approved" do
+    shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                               address: '1234 Happy Lane',
+                               city: 'Hopscotch Town',
+                               state: 'Colorado',
+                               zip: 12345)
+    user = User.create!(name: "Betty",
+                        address: "123 Main st",
+                        city: "Denver",
+                        state: "CO",
+                        zip: 80111)
+    pet_1 = Pet.create(image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                       name: 'Skye',
+                       approx_age: 3,
+                       sex: 'Female',
+                       shelter_id: shelter_1.id)
+    application = Application.create!(description: "I will take great care of Skye",
+                                      status: "Approved",
+                                      user_id: user.id)
+    pet_application = PetApplication.create!(pet_id: pet_1.id,
+                                             user_id: user.id,
+                                             application_id: application.id)
+
+    expect(pet_1.apps_approved?).to eq(true)
+  end
+
+  it "knows if apps are NOT approved" do
+    shelter_1 = Shelter.create(name: 'Sunny Days Shelter',
+                               address: '1234 Happy Lane',
+                               city: 'Hopscotch Town',
+                               state: 'Colorado',
+                               zip: 12345)
+    user = User.create!(name: "Betty",
+                        address: "123 Main st",
+                        city: "Denver",
+                        state: "CO",
+                        zip: 80111)
+    pet_1 = Pet.create(image: 'https://i.ibb.co/JzcLkB6/pet-1.jpg',
+                       name: 'Skye',
+                       approx_age: 3,
+                       sex: 'Female',
+                       shelter_id: shelter_1.id)
+    application = Application.create!(description: "I will take great care of Skye",
+                                      status: "In Progress",
+                                      user_id: user.id)
+    pet_application = PetApplication.create!(pet_id: pet_1.id,
+                                             user_id: user.id,
+                                             application_id: application.id)
+
+    expect(pet_1.apps_approved?).to eq(false)
+  end
 end
