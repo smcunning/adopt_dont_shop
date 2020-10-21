1e09b275e3a869bb91fc6d2619d03a37bc49d6b3
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-10-21 15:22:45 -0600

    Change method name in final spec to apps_approved

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -145,7 +145,7 @@ describe Shelter, type: :model do
                                              application_id: application_2.id)
 
 
-    expect(shelter_1.any_pets_approved?).to eq(true)
-    expect(shelter_2.any_pets_approved?).to eq(false)
+    expect(shelter_1.apps_approved?).to eq(true)
+    expect(shelter_2.apps_approved?).to eq(false)
   end
 end
